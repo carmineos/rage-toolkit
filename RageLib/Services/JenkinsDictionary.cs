92be2118ee522c3586e37ad7cd4f8ff3faf1b093
carmineos <neos7@live.it>
2021-04-20 18:28:09 +0200

    JenkinsDictionary: add AddFromFile api

--- RageLib/Services/JenkinsDictionary.cs
+++ RageLib/Services/JenkinsDictionary.cs
@@ -1,5 +1,6 @@
 ﻿using RageLib.Hash;
 using System.Collections.Generic;
+using System.IO;
 
 namespace RageLib.Services
 {
@@ -8,6 +9,24 @@ namespace RageLib.Services
         public bool TryAdd(string data)
         {
             return this.TryAdd(Jenkins.Hash(data), data);
+        }
+
+        public void AddFromFile(string filePath)
+        {
+            using (Stream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                AddFromFile(fileStream);
+        }
+
+        public void AddFromFile(Stream stream)
+        {
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string name = reader.ReadLine();
+                    TryAdd(name);
+                }
+            }
         }
     }
 }
