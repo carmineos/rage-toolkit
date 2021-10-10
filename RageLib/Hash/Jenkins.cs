4a9c3470b0aa56c6c5a17aec3ece0a20ff87d531
carmineos <neos7@live.it>
2021-10-10 16:46:23 +0200

    Jenkins: remove duplicated code in Hash overload

--- RageLib/Hash/Jenkins.cs
+++ RageLib/Hash/Jenkins.cs
@@ -29,17 +29,7 @@ namespace RageLib.Hash
         // source: http://en.wikipedia.org/wiki/Jenkins_hash_function
         public static uint Hash(string key)
         {
-            uint hash = 0;
-            for (int i = 0; i < key.Length; ++i)
-            {
-                hash += (byte)key[i];
-                hash += (hash << 10);
-                hash ^= (hash >> 6);
-            }
-            hash += (hash << 3);
-            hash ^= (hash >> 11);
-            hash += (hash << 15);
-            return hash;
+            return Hash(key.AsSpan());
         }
 
         public static uint Hash(ReadOnlySpan<char> key)
