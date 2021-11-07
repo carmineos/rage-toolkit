6dc3f0b521392fdb0a845869a08702d3f5307ebf
carmineos <neos7@live.it>
2021-11-07 12:23:02 +0100

    Resources: move DatResourceInfo to own file

--- RageLib/Resources/DatResourceFileHeader.cs
+++ RageLib/Resources/DatResourceFileHeader.cs
@@ -14,19 +14,4 @@
             ResourceInfo = new DatResourceInfo(virtualFlags, physicalFlags);
         }
     }
-
-    // datResourceInfo
-    public struct DatResourceInfo
-    {
-        public uint VirtualFlags;
-        public uint PhysicalFlags;
-
-        public DatResourceInfo(uint virtualFlags, uint physicalFlags)
-        {
-            VirtualFlags = virtualFlags;
-            PhysicalFlags = physicalFlags;
-        }
-
-        public int Version => (int)(((PhysicalFlags & 0xF0000000) >> 28) | (VirtualFlags & 0xF0000000) >> 24);
-    }
 }
---
+++ RageLib/Resources/DatResourceInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace RageLib.Resources
+{
+    // datResourceInfo
+    public struct DatResourceInfo
+    {
+        public uint VirtualFlags;
+        public uint PhysicalFlags;
+
+        public DatResourceInfo(uint virtualFlags, uint physicalFlags)
+        {
+            VirtualFlags = virtualFlags;
+            PhysicalFlags = physicalFlags;
+        }
+
+        public int Version => (int)(((PhysicalFlags & 0xF0000000) >> 28) | (VirtualFlags & 0xF0000000) >> 24);
+    }
+}
