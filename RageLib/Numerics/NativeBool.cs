46b986c21c51e058fc1a2a0711956e92f9bc8bc1
carmineos <neos7@live.it>
2021-11-17 22:07:44 +0100

    RageLib: add NativeBool struct

---
+++ RageLib/Numerics/NativeBool.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace RageLib.Numerics
+{
+    public struct NativeBool
+    {
+        private readonly byte value;
+
+        public NativeBool(bool boolean)
+        {
+            value = (byte)(boolean ? 1 : 0);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator true(NativeBool value) => value.value != 0;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool operator false(NativeBool value) => value.value == 0;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator bool(NativeBool nativeBool) => nativeBool.value != 0;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static implicit operator NativeBool(bool boolean) => new NativeBool(boolean);
+    }
+}
