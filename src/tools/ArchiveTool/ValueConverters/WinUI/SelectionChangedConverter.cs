133eb768cb6469f6a19a70d27b068e31bb89dcc9
carmineos <neos7@live.it>
2024-09-14 20:59:08 +0200

    replace CommunityToolkit.WinUI.UI.Controls.DataGrid with CommunityToolkit.Labs.WinUI.Controls.DataTable

--- src/tools/ArchiveTool/ValueConverters/WinUI/SelectionChangedConverter.cs
+++ src/tools/ArchiveTool/ValueConverters/WinUI/SelectionChangedConverter.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright © Neodymium, carmineos and contributors. See LICENSE.md in the repository root for more information.
 
-using CommunityToolkit.WinUI.UI.Controls;
+using CommunityToolkit.WinUI.Controls;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Data;
 using System;
@@ -14,7 +14,7 @@ namespace ArchiveTool.ValueConverters.WinUI
             return parameter switch
             {
                 ListView listView => listView.SelectedItems,
-                DataGrid gridView => gridView.SelectedItems,
+                //DataGrid gridView => gridView.SelectedItems,
                 _ => null,
             };
         }
